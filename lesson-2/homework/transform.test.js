fdd15ad09f4e65b077b3360addaae14b339ccd87
Vlad Khitev <vlad.khitev@axondevgroup.com>
2019-03-29 11:40:28 +0200

    Levels of complexity

--- lesson-2/homework/transform.test.js
+++ lesson-2/homework/transform.test.js
@@ -3,21 +3,21 @@ const transform = require('./transform')
 const add = a => b => a + b
 
 describe('transform', () => {
-  it('creates a new object by transforming the `object` according to the `transformation` functions', () => {
+  it('1. creates a new object by transforming the `object` according to the `transformation` functions', () => {
     const transf = { elapsed: add(1), remaining: add(-1) }
     const object = { name: 'Tomato', elapsed: 100, remaining: 1400 }
     const expected = { name: 'Tomato', elapsed: 101, remaining: 1399 }
     expect(transform(transf, object)).toEqual(expected)
   })
 
-  it('does not invoke function if object does not contain the key', () => {
+  it('1. does not invoke function if object does not contain the key', () => {
     const transf = { n: add(1), m: add(1) }
     const object = { m: 3 }
     const expected = { m: 4 }
     expect(transform(transf, object)).toEqual(expected)
   })
 
-  it('is not destructive', () => {
+  it('1. is not destructive', () => {
     const transf = { elapsed: add(1), remaining: add(-1) }
     const object = { name: 'Tomato', elapsed: 100, remaining: 1400 }
     const expected = { name: 'Tomato', elapsed: 100, remaining: 1400 }
@@ -25,28 +25,28 @@ describe('transform', () => {
     expect(object).toEqual(expected)
   })
 
-  it('is recursive', () => {
-    const transf = { nested: { second: add(-1), third: add(1) } }
-    const object = { first: 1, nested: { second: 2, third: 3 } }
-    const expected = { first: 1, nested: { second: 1, third: 4 } }
-    expect(transform(transf, object)).toEqual(expected)
-  })
-
-  it('ignores primitive value transformations', () => {
+  it('2. ignores primitive value transformations', () => {
     const transf = { n: 2, m: 'foo' }
     const object = { n: 0, m: 1 }
     const expected = { n: 0, m: 1 }
     expect(transform(transf, object)).toEqual(expected)
   })
 
-  it('ignores null transformations', () => {
+  it('2. ignores null transformations', () => {
     const transf = { n: null }
     const object = { n: 0 }
     const expected = { n: 0 }
     expect(transform(transf, object)).toEqual(expected)
   })
 
-  it('creates a new array by transforming the `array` according to the `transformation` functions', () => {
+  it('3. is recursive', () => {
+    const transf = { nested: { second: add(-1), third: add(1) } }
+    const object = { first: 1, nested: { second: 2, third: 3 } }
+    const expected = { first: 1, nested: { second: 1, third: 4 } }
+    expect(transform(transf, object)).toEqual(expected)
+  })
+
+  it('4. creates a new array by transforming the `array` according to the `transformation` functions', () => {
     const transf = [add(1), add(-1)]
     const object = [100, 1400]
     const expected = [101, 1399]
